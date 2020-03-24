6a25a9993b0fc000aad915550da00e1bd29de01e
Andrei Klimenok <jaguarislove@gmail.com>
2020-03-24 11:03:48 +0100

    adjust random int generator

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const generateRandomInteger = (stNum, endNum) => Math.floor(Math.random() * endNum + 1) + stNum;
+const genRandomInt = (stNum, endNum) => Math.floor(Math.random() * (endNum - stNum + 1) + stNum);
 
-export default generateRandomInteger;
+export default genRandomInt;
